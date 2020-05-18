cb64f0b026eb62e5fcb086b705b1d78dfdae60a0
Quentin Gouazé <quentin.gouaze@campus-igs-toulouse.fr>
2020-05-18 13:40:03 +0200

    End Tutorial

--- client/index.js
+++ client/index.js
@@ -1,5 +1,21 @@
 
 window.onload=()=>{
+const socketConnection=new WebSocket('ws://www.localhost:3000/');
+socketConnection.onopen=(connectionEvent)=>{
+    console.log('websocket connection opened', connectionEvent);
+};
+socketConnection.onmessage = (messageObject) => {
+    const buttonClicked= document.getElementById(messageObject.data);
+    if (buttonClicked.innerHTML==='-'){
+        buttonClicked.innerHTML="O";
+        buttonClicked.disabled=true;
+    }
+};
+socketConnection.onerror=(error)=> {
+    console.log('socket error:', error);
+};
+
+
  const collectionOfButtons=document.querySelectorAll('button');
  let xTurn=true;
  collectionOfButtons.forEach((buttonElement)=>{
--- server/server.js
+++ server/server.js
@@ -1,9 +1,31 @@
 const express=require('express');
 const path=require('path');
 const http=require('http');
+const WebSocket=require("ws");
 
 const app=express();
 const server=http.createServer(app);
+const wsServer=new WebSocket.Server({ server });
+
+const setClients=new Set();
+
+wsServer.on('connection', (socketConnection)=>{
+    setClients.add(socketConnection);
+    console.log('New client connected, total connections: ', setClients.size);
+
+    socketConnection.on('message', (message)=>{
+        setClients.forEach((oneClient)=>{
+            oneClient.send(message);
+        });
+});
+socketConnection.on('close',()=> {
+    setClients.delete(socketConnection);
+    console.log("Client disconnected, total connections: ", setClients.size);
+});
+
+});
+
+
 
 app.use(express.static(path.resolve(__dirname,'../client')));
 
